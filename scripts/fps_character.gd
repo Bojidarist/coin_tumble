9bd32b377baba2491a984ce0abccb98a2f4238e7
Bojidar Qnkov <bojidar.qnkov@gmail.com>
2026-04-13 17:54:22 +0300

    Shoot sfx and SFX sound bus

--- scripts/fps_character.gd
+++ scripts/fps_character.gd
@@ -2,10 +2,12 @@ extends Node3D
 
 @export var mouse_sensitivity: float = 0.001
 @onready var ray_cast_3d: RayCast3D = $RayCast3D
+@onready var sound_bounce: AudioStreamPlayer = $SoundBounce
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	if Input.is_action_just_pressed("shoot"):
+		sound_bounce.play()
 		if ray_cast_3d.is_colliding():
 			var collider_name = ray_cast_3d.get_collider().get_name()
 			print(collider_name)
